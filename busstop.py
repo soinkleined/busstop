cafdf36d1ab36d061b5a9a6f97af3f73e447ed9e
David Klein <david@soinkleined.com>
2021-12-13 17:57:26 +0000

    non-working debu

--- busstop.py
+++ busstop.py
@@ -12,28 +12,40 @@ https://api.tfl.gov.uk/swagger/ui/index.html#!/StopPoint/StopPoint_MetaCategorie
 490015396S -> Newington Green
 490005432S2 -> Clissold Crescent
 '''
-debug = 0
+
+'''
+https://pypi.org/project/termcolor/
+Can be one of the following or None
+grey red green yellow blue magenta cyan white
+color = None
+'''
+color = 'yellow'
 
 def getStopName(id):
     r = requests.get('https://api.tfl.gov.uk/StopPoint/' + id)
     json_result = r.json()
     stop_name=json_result['commonName']
     return(stop_name)
-    if debug ==1:
-        print(json.dumps(json_result, indent=4, sort_keys=True))
+    print(json.dumps(json_result, indent=4, sort_keys=True))
 
 def getBusTime(id):
     now = dt.now(timezone('Europe/London'))
     date_format = "%Y-%m-%d"
     time_format  = "%H:%M:%S"
-    date_and_time = now.strftime(date_format  + " " + time_format) 
     r = requests.get('https://api.tfl.gov.uk/StopPoint/' + id + '/Arrivals')
     json_result = r.json()
     json_result.sort(key = lambda x:x["expectedArrival"])
-    stop_name=getStopName(id)
     my_stops = []
-    my_stops.append(stop_name.center(56))
-    my_stops.append(date_and_time.center(56))
+    if color:
+        stop_name=colored(getStopName(id),color)
+        date_and_time = colored(now.strftime(date_format  + " " + time_format), color) 
+        my_stops.append(stop_name.center(62)) # termcolor adds characters
+        my_stops.append(date_and_time.center(62))
+    else:
+        stop_name=getStopName(id)
+        date_and_time = now.strftime(date_format  + " " + time_format) 
+        my_stops.append(stop_name.center(56))
+        my_stops.append(date_and_time.center(56))
     num = 0
     for x in json_result:
           due_in=''
@@ -45,19 +57,20 @@ def getBusTime(id):
               due_in = 'due'
           else:
               due_in = str(away_min) + 'min'
-          x = "%3s %4s %30s %9s %6s" % (str(num),str(x['lineName']),str(x['destinationName']),arrival_time,due_in)
+          if color:
+              x = colored("%3s %4s %30s %9s %6s" % (str(num),str(x['lineName']),str(x['destinationName']),arrival_time,due_in), color)
+          else:
+              x = "%3s %4s %30s %9s %6s" % (str(num),str(x['lineName']),str(x['destinationName']),arrival_time,due_in)
           my_stops.append(x)
     if num == 0:
-        x = 'No information at this time.'    
+        x = colored('No information at this time.',color)    
         my_stops.append(x)
     return(my_stops)
-    if debug ==1:
-        print(json.dumps(json_result, indent=4, sort_keys=True))
 
 def main(argv):
     for x in argv[1:]:
         for stop in(getBusTime(x)):
-            print(colored(stop, 'yellow'))
+            print(stop)
         print()
 
 if __name__ == "__main__":
