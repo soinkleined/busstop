61b88e4639cd2fba722f8a17de89f926d915d1cd
David Klein <david@soinkleined.com>
2021-12-14 20:44:06 +0000

    modified:   README.md - updated information for web output 	modified:   busstop.py - added web output 	web/

--- README.md
+++ README.md
@@ -9,10 +9,25 @@ Using the TFL API, find the corresponding StopPoint(s) and provide it/them as (a
 
 ![busstop](https://raw.githubusercontent.com/soinkleined/busstop/main/images/busstop.png)
 
+The script now also renders html output including dot matrix style webfonts.
+
+# To Do
+1. optimize css code
+2. add licenses and missing credits
+3. setup on rpi with chromium kiosk mode
+4. add better handling of formatting arguments
+5. handle errors for connectivity, api availability, etc.
+
 ## Acknowledgements
 Balena.io is a very interesting platform for IoT fleet management.  I saw their blog post about creating a live train time sign under your monitor and was inspired to do something similar for busses. There are a lot of links to other resources from the post and I hope to also do this from a pi zero with a custom case at some point.
 
 https://www.balena.io/blog/build-a-raspberry-pi-powered-train-station-oled-sign-for-your-desk/
 
+I used the fonts from Sean Petykowski and created webfonts via Font Squirrel. Thank you very much!
 
+Please see the original ttf fonts here:
+
+https://github.com/petykowski/London-Underground-Dot-Matrix-Typeface
+
+https://www.fontsquirrel.com/tools/webfont-generator
 
--- busstop.py
+++ busstop.py
@@ -15,18 +15,19 @@ https://api.tfl.gov.uk/swagger/ui/index.html#!/StopPoint/StopPoint_MetaCategorie
 
 '''
 https://pypi.org/project/termcolor/
-Can be one of the following or None
+Can be one of the following:
 grey red green yellow blue magenta cyan white
+or unset it: 
 color = None
 '''
-color = 'yellow'
+color='yellow'
+web=1
 
 def getStopName(id):
     r = requests.get('https://api.tfl.gov.uk/StopPoint/' + id)
     json_result = r.json()
     stop_name=json_result['commonName']
     return(stop_name)
-    print(json.dumps(json_result, indent=4, sort_keys=True))
 
 def getBusTime(id):
     now = dt.now(timezone('Europe/London'))
@@ -36,7 +37,12 @@ def getBusTime(id):
     json_result = r.json()
     json_result.sort(key = lambda x:x["expectedArrival"])
     my_stops = []
-    if color:
+    if web:
+        stop_name=getStopName(id)
+        date_and_time = now.strftime(date_format  + " " + time_format)
+        my_stops.append('        <span class="stop_name">' + stop_name + '</span></br>')
+        my_stops.append('        <span class="date_and_time">' + date_and_time + '</span></br>')
+    elif color:
         stop_name=colored(getStopName(id),color)
         date_and_time = colored(now.strftime(date_format  + " " + time_format), color) 
         my_stops.append(stop_name.center(62)) # termcolor adds characters
@@ -57,7 +63,9 @@ def getBusTime(id):
               due_in = 'due'
           else:
               due_in = str(away_min) + 'min'
-          if color:
+          if web:
+              x =  '        <span class="number">' + str(num) + '</span><span class="lineName">' + str(x['lineName']) + '</span><span class="destinationName">' + str(x['destinationName']) + '</span><span class="arrival_time">' + arrival_time + '</span><span class="due_in">' + due_in + '</span></br>'
+          elif color:
               x = colored("%3s %4s %30s %9s %6s" % (str(num),str(x['lineName']),str(x['destinationName']),arrival_time,due_in), color)
           else:
               x = "%3s %4s %30s %9s %6s" % (str(num),str(x['lineName']),str(x['destinationName']),arrival_time,due_in)
@@ -67,11 +75,37 @@ def getBusTime(id):
         my_stops.append(x)
     return(my_stops)
 
+def printWeb(top_or_bottom):
+    if top_or_bottom == 'top':
+        print("""<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+	\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
+<head>
+	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
+	<link rel=\"stylesheet\" href=\"css/font_stylesheet.css\" type=\"text/css\" charset=\"utf-8\" />
+	<link rel=\"stylesheet\" href=\"css/stylesheet.css\" type=\"text/css\" charset=\"utf-8\" />
+	<style type=\"text/css\">
+					body{
+				font-family: \"london_undergroundregular\";
+							}
+		</style>
+	<title>busstop</title>
+</head>
+<body>
+    <div id=\"container\">""")
+    elif top_or_bottom == 'bottom':
+        print("""    </div> 
+</body>""")
+
 def main(argv):
+    if web:
+        printWeb('top')
     for x in argv[1:]:
         for stop in(getBusTime(x)):
             print(stop)
         print()
+    if web:
+        printWeb('bottom')
 
 if __name__ == "__main__":
     main(sys.argv)
