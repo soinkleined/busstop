441da9dbc4b4a1f3d8023acd865fb46ffdee426d
David Klein <dklein@irwin.local>
2021-12-28 22:06:07 +0000

    new file:   kiosk_files/run_busstop_browser.sh 	new file:   kiosk_files/splash.png

---
+++ kiosk_files/run_busstop_browser.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+SITE="http://127.0.0.1:5000"
+UPDATE_INTERVAL="31536000" # 31536000 seconds = 1 year
+/usr/bin/chromium-browser --app="${SITE}" \
+    --kiosk \
+    --noerrdialogs \
+    --disable-session-crashed-bubble \
+    --disable-restore-session-state \
+    --disable-infobars \
+    --check-for-update-interval="${UPDATE_INTERVAL}" \
+    --disable-pinch
