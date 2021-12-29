fdb1911348ac3f04ca14ba8f9ae47654e059d7b4
David Klein <david@soinkleined.com>
2021-12-29 18:48:02 +0000

    modified:   kiosk_files/run_busstop_browser.sh 	kiosk_files/busstop.service 	kiosk_files/install_packages.sh 	kiosk_files/setup_kiosk.sh

--- kiosk_files/run_busstop_browser.sh
+++ kiosk_files/run_busstop_browser.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SITE="http://127.0.0.1:5000"
+SITE="http://127.0.0.1:8000"
 UPDATE_INTERVAL="31536000" # 31536000 seconds = 1 year
 /usr/bin/chromium-browser --app="${SITE}" \
     --kiosk \
