74524459ba5c6569dd5e4744565c5435f17a936a
David Klein <david@soinkleined.com>
2022-01-12 17:25:48 +0000

    deleted:    busstop.service 	new file:   busstop_client.service 	new file:   busstop_server.service 	deleted:    run_busstop_browser.sh 	modified:   setup_kiosk.sh

--- kiosk_files/setup_kiosk.sh
+++ kiosk_files/setup_kiosk.sh
@@ -1,6 +1,15 @@
+##############################
+# Vars
+##############################
+NEW_HOSTNAME='busstop'
+CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 AUTOSTART="/etc/xdg/lxsession/LXDE-pi/autostart"
 SPLASH="/usr/share/plymouth/themes/pix/splash.png"
+
+##############################
+# Set browser kiosk startup
+##############################
 mv  "${AUTOSTART}" "${AUTOSTART}.${TIMESTAMP}"
 
 echo "# Configured by busstop on ${TIMESTAMP}
@@ -8,9 +17,18 @@ echo "# Configured by busstop on ${TIMESTAMP}
 @pcmanfm --desktop --profile LXDE-pi
 @xset s off
 @xset -dpms
-@xset s noblank
+@xset s noblank" > "${AUTOSTART}"
 
-@/home/pi/busstop/kiosk_files/run_busstop_browser.sh" > "${AUTOSTART}"
-
+##############################
+# Set splash screen
+##############################
 mv "${SPLASH}" "${SPLASH}.${TIMESTAMP}"
 cp ./splash.png "${SPLASH}"
+
+##############################
+# Set hostname
+##############################
+cp /etc/hostname "/etc/hostname.${TIMESTAMP}"
+cp /etc/hosts "/etc/hosts.${TIMESTAMP}"
+echo $NEW_HOSTNAME > /etc/hostname
+sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
