0ba67edd95db61e0c9fa29594e9c35ac0dbd50fa
David Klein <david@soinkleined.com>
2023-09-16 10:57:57 +0100

    modified:   Dockerfile - added debug 	modified:   busstop.py - pep8 formatting 	modified:   gunicorn_run.sh - added debug

--- flask_app/gunicorn_run.sh
+++ flask_app/gunicorn_run.sh
@@ -1,1 +1,1 @@
-venv/bin/gunicorn --bind localhost:8000 --workers 1 --threads 5  --log-level=debug main:app
+venv/bin/gunicorn --bind localhost:8001 --workers 1 --threads 5  --log-level=debug main:app
