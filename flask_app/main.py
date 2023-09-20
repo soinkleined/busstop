fa5c90e0881b59b23b5d61ef7797e863e0fed91f
David Klein <david@soinkleined.com>
2023-09-20 12:26:49 +0100

    modified:   flask_app/busstop.py - pycharm issues addressed 	modified:   flask_app/config.ini - pycharm issues addressed 	modified:   flask_app/main.py - pycharm issues addressed

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,11 +1,11 @@
-'''
+"""
 https://github.com/realpython/flask-boilerplate
-'''
+"""
 import logging
 from logging import Formatter, FileHandler
 import threading
 import time
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from turbo_flask import Turbo
 from busstop import get_stops
 
@@ -21,7 +21,7 @@ if __name__ != '__main__':
 
 
 def update_stops():
-    '''update stops'''
+    """update stops"""
     with app.app_context():
         while True:
             time.sleep(UPDATE_INTERVAL)
@@ -31,26 +31,50 @@ def update_stops():
 
 @app.context_processor
 def get_all_stops():
-    '''start getting stops'''
+    """start getting stops"""
     all_stops = get_stops()
     return {"all_stops": all_stops}
 
 
 @app.route("/")
 def index():
-    '''render index template'''
+    """render index template"""
     return render_template("index.html")
 
 
+@app.route("/admin")
+def admin():
+    """render admin template"""
+    with open('config.ini', 'r') as file:
+        config_data = file.read()
+    return render_template('admin.html', config_data=config_data)
+
+
+@app.route("/admin", methods=['POST'])
+def admin_post():
+    """render admin post template"""
+    config_data = request.form['config_data']
+    if request.method == 'POST':
+        with open('config.ini', 'w') as f:
+            f.write(str(config_data))
+    return render_template('admin.html', nopol=config_data)
+
+
+@app.route("/about")
+def about():
+    """render about template"""
+    return render_template("about.html")
+
+
 @app.errorhandler(500)
-def internal_error(error):
-    '''render 500 page'''
+def internal_error():
+    """render 500 page"""
     return render_template('errors/500.html'), 500
 
 
 @app.errorhandler(404)
-def not_found_error(error):
-    '''render 404 page'''
+def not_found_error():
+    """render 404 page"""
     return render_template('errors/404.html'), 404
 
 
