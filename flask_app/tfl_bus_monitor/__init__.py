67d7b87b985a0499339c9010bd96a541e9c205b6
David Klein <david@soinkleined.com>
2023-09-28 09:44:36 +0100

    modified:   flask_app/tfl_bus_monitor/__init__.py - added author and version info

--- flask_app/tfl_bus_monitor/__init__.py
+++ flask_app/tfl_bus_monitor/__init__.py
@@ -1,5 +1,7 @@
 """
 tfl_bus_monitor - download bus data from TFL
 """
+__author__ = 'David Klein'
+__version__ = '0.1'
 from .tfl_bus_monitor import TFLBusMonitor
 get_stops = TFLBusMonitor().get_stops
