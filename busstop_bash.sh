db15a22fa9d1ad64fe74c1c301b94868f6522e44
David Klein <david@soinkleined.com>
2023-09-30 17:08:20 +0100

    modified:   busstop_bash.sh

--- busstop_bash.sh
+++ busstop_bash.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 SLEEP=15
+if busstop -h >/dev/null 2>&1; then
+    :
+else
+    echo "'busstop' is not installed.  Please install using 'pip install ./tfl_bus_monitor'." && exit 1
+fi
+
 while true; do
     output=$(busstop -t 2>/dev/null) 
     if [ -n "${output}" ]; then
