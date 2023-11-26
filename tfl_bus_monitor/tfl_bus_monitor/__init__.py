79c3dc4e55447d7f6f2ff5dd1f8c5d5575a8c991
David Klein <david@soinkleined.com>
2023-11-26 19:17:11 +0000

    deleted:    ../tfl_bus_monitor/README.md 	deleted:    ../tfl_bus_monitor/setup.py 	deleted:    ../tfl_bus_monitor/tfl_bus_monitor/__init__.py 	deleted:    ../tfl_bus_monitor/tfl_bus_monitor/config/busstop_config.ini 	deleted:    ../tfl_bus_monitor/tfl_bus_monitor/tfl_bus_monitor.py

--- tfl_bus_monitor/tfl_bus_monitor/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-tfl_bus_monitor - download bus data from TFL
-"""
-__author__ = 'David Klein'
-__email__ = 'david@soinkleined.com'
-__version__ = '0.4'
-from .tfl_bus_monitor import TFLBusMonitor
-get_stops = TFLBusMonitor().get_stops
-get_config_path = tfl_bus_monitor.get_config_path()
