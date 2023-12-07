50bc75a9167cb40e2e4122457285e17a07eec82c
David Klein <david@soinkleined.com>
2023-12-07 15:53:11 +0000

    modified:   busstop_bash.sh - updated to reflect pypi package

--- busstop_bash.sh
+++ busstop_bash.sh
@@ -3,7 +3,7 @@ SLEEP=15
 if busstop -h >/dev/null 2>&1; then
     :
 else
-    echo "'busstop' is not installed.  Please install using 'pip install ./tfl_bus_monitor'." && exit 1
+    echo "'busstop' is not installed.  Please install using 'pip install tfl_bus_monitor'." && exit 1
 fi
 
 while true; do
